bf0d9a38fa051dace731e387c67169d90c61314b
bellag10 <140200750+bellag10@users.noreply.github.com>
2024-10-27 09:32:23 -0500

    updated program

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+function toggleTheme() {
+    document.body.classList.toggle("light-theme");
+}
 function toggleDetails(eventElement) {
     eventElement.classList.toggle("expanded");
 }
-  
+function openImageWithCaption(imageSrc, captionText) {
+    const viewerUrl = `image-viewer.html?image=${encodeURIComponent(imageSrc)}&caption=${encodeURIComponent(captionText)}`;
+    window.open(viewerUrl, '_blank');
+}
