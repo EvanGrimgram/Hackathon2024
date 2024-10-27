6877b2da54f3b18dd5c5414632ac7a065c7217b4
bellag10 <140200750+bellag10@users.noreply.github.com>
2024-10-26 21:25:50 -0500

    update, support multiple browsers and non-defaulted browsers

--- Main.java
+++ Main.java
@@ -1,14 +1,37 @@
-/* files: home.html...  FashionTimeline.java is dead */
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        /* FashionTimeline.main(args); dead code */
-
-        /* Open the HTML file in a browser */
+        String filePath = new File("home.html").getAbsolutePath();
+        if (!openWithDefaultBrowser(filePath)) {
+            if (!openWithSpecificBrowser("C:\\Program File\\Google\\Chrome\\Application\\chrome.exe", filePath)) {
+                if (!openWithSpecificBrowser("C:\\Program File (x86)\\Microsoft\\Edge\\Application\\msedge.exe", filePath)) {
+                    openWithSpecificBrowser("C:\\Program File\\Mozilla Firefox\\firefox.exe", filePath);
+                }
+            }
+        }
+    }
+    
+    private static boolean openWithDefaultBrowser(String filePath) {
         try {
-            java.awt.Desktop.getDesktop().browse(new java.io.File("home.html").toURI()); //allows Java applications to interact with the desktop environment, such as opening a file in a browser
-        } catch (Exception e) {
-            e.printStackTrace(); //If an error occurs, the catch block will print the stack trace of the exception.
+            Desktop.getDesktop().browse(new File(filePath).toURI());
+            return true;
+        }
+        catch (IOException e) {
+            return false;
+        }
+    }
+    private static boolean openWithSpecificBrowser(String browserPath, String filePath) {
+        try {
+            Process process = Runtime.getRuntime().exec(new String[]{browserPath, filePath});
+            process.getErrorStream().close();
+            return true;
+         } catch (IOException e) {
+            return false;
+         
+
         }
     }
 }
